c8042d78029b8e8713477d1470c1ee4547a15011
Natanssilva <natanssilva10@gmail.com>
2023-12-01 19:17:18 -0300

    arrumando telas de login e cadastro

--- src/cadastro.php
+++ src/cadastro.php
@@ -3,53 +3,60 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Newspaper</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet">
-    <script src="../js/validarCadastro.js" defer></script>
-    <script src="../js/main.js" defer></script>
+    <link rel="stylesheet" href="../dist/output.css">
+    <script src="../js/login.js" defer></script>
 </head>
 
-<body class="bg-gray-800 min-h-screen flex items-center justify-center">
-    <div class="bg-white p-8 rounded shadow-md sm:w-96 animate__animated animate__backInRight ">
-        <h2 class="text-2xl font-bold mb-4 text-center">Cadastre-se</h2>
-        <form id="formCadastro" action="processar_cadastro.php">
-            <div class="mb-2">
+<body class="bg-black-800 min-h-screen flex items-center justify-center">
+
+    <div x-data="{ show: true }" x-show="show" x-transition:enter="animate__animated animate__backInRight" class="bg-white p-8 rounded shadow-md sm:w-96 animate__animated animate__backInRight mx-auto">
+        <h2 class="text-2xl font-bold mb-4 text-center">Cadastro</h2>
+        <form id="form" action="processar_login.php" method="post">
+            <div class="mb-4">
                 <label for="nome" class="block text-gray-700 text-sm font-semibold mb-2">Nome</label>
-                <input type="text" id="nome" name="nomeCadastro" class="border rounded w-full py-2 px-3" placeholder="Digite seu nome">
+
+                <input type="text" id="nome" name="nome" class="input w-full bg-white" placeholder="seuemail@exemplo.com">
+
                 <p class="error-message text-red-700"></p>
             </div>
-            <div class="mb-2">
+            <div class="mb-4">
                 <label for="sobrenome" class="block text-gray-700 text-sm font-semibold mb-2">Sobrenome</label>
-                <input type="text" id="sobrenome" name="sobrenomeCadastro" class="border rounded w-full py-2 px-3" placeholder="Digite seu sobrenome">
+
+                <input type="text" id="sobrenome" name="sobrenome" class="input w-full bg-white" placeholder="seuemail@exemplo.com">
+
+                <p class="error-message text-red-700"></p>
             </div>
             <div class="mb-4">
                 <label for="email" class="block text-gray-700 text-sm font-semibold mb-2">Email</label>
-                <input type="email" id="emailCadastro" name="emailCadastro" class="border rounded w-full py-2 px-3" placeholder="seuemail@exemplo.com">
-                <p class="error-message text-red-700"></p>
-            </div>
-            <div class="relative rounded-md shadow-sm mb-6">
-                <label for="senha" class="block text-gray-700 text-sm font-semibold mb-2">Senha</label>
-                <input type="password" id="senhaCadastro" name="senhaCadastro" class="senha border rounded w-full py-2 px-3" placeholder="********">
-                <div class="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer mt-6">
-                    <img class="hide-password" src="../images/icons8-hide-password-30.png" alt="hide-password">
-                    <img class="show-password hidden" src="../images/icons8-show-password-30.png" alt="show-password">
-                </div>
-                <p class="error-message text-red-700"></p>
 
+                <input type="email" id="email" name="email" class="input w-full bg-white" placeholder="seuemail@exemplo.com">
+
+                <p class="error-message text-red-700"></p>
             </div>
-            <button id="buttonCadastro" type="submit" class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full py-2">Cadastrar</button>
-            <p class="cadastro-error text-red-700"></p>
+            <div class="mb-6 relative">
+                <label for="senha" class="block text-gray-700 text-sm font-semibold mb-2">Senha</label>
+
+                <div class="relative rounded-md shadow-sm">
+                    <input type="password" id="senha" name="senha" class="input w-full bg-white" placeholder="********">
+                    <div class="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer">
+                        <img class="hide-password" src="../image/icons8-hide-password-30.png" alt="hide-password">
+                        <img class="show-password hidden" src="../image/icons8-hide-password-30.png" alt="show-password">
+                    </div>
+                    <p class="erro-login text-red-700"></p>
+                </div>
+            </div>
+            <div class="text-center">
+                <button id="button-submit" type="submit" class="btn bg-black hover:bg-white text-white hover:text-black font-bold py-1 px-4 border-b-4 border-black  rounded w-full py-1">Entrar</button>
+            </div>
         </form>
         <div class="mt-4">
-            <p class="text-gray-600">Já possui uma conta? <a href="index.php" class="hover:underline text-blue-500">Fazer login</a></p>
-
+            <p class="text-gray-600 px-2 ">Já possui login? <a href="index.php" class="hover:underline text-black-500">Faça Login agora mesmo</a></p>
         </div>
-    </div>
 
-    <h1 class="text-2xl text-white text-center absolute top-10 w-full animate__animated animate__backInRight ">Crie uma conta e comece agora mesmo a gerenciar suas finanças !</h1>
-    <div id="alert"></div>
+
 </body>
 
 </html>
--- src/index.php
+++ src/index.php
@@ -10,61 +10,50 @@
     <script src="../js/login.js" defer></script>
 </head>
 
-<body>
-    <body class="bg-black-800 min-h-screen flex items-center justify-center ">
+<body class="bg-black-800 min-h-screen flex items-center justify-center">
 
-        <h1 class="text-2xl text-white text-center absolute top-10 w-full animate__animated animate__backInRight ">Seja
-            Bem vindo ao Newspaper !</h1>
+    <h1 x-data="{ show: true }" x-show="show" x-transition:enter="animate__animated animate__backInRight" class="text-2xl text-white text-center absolute top-10 w-full">Seja Bem vindo ao Newspaper !</h1>
 
-        <div class="bg-white p-8 rounded shadow-md sm:w-96 animate__animated animate__backInRight mx-auto">
-            <h2 class="text-2xl font-bold mb-4 text-center">Login</h2>
-            <form id="form" action="processar_login.php" method="post">
-                <div class="mb-4">
-                    <label for="email" class="block text-gray-700 text-sm font-semibold mb-2">Email</label>
+    <div x-data="{ show: true }" x-show="show" x-transition:enter="animate__animated animate__backInRight" class="bg-white p-8 rounded shadow-md sm:w-96 animate__animated animate__backInRight mx-auto">
+        <h2 class="text-2xl font-bold mb-4 text-center">Login</h2>
+        <form id="form" action="processar_login.php" method="post">
+            <div class="mb-4">
+                <label for="email" class="block text-gray-700 text-sm font-semibold mb-2">Email</label>
 
-                    <input type="email" id="email" name="email" class="border rounded w-full py-2 px-3"
-                        placeholder="seuemail@exemplo.com">
+                <input type="email" id="email" name="email" class="input w-full bg-white" placeholder="seuemail@exemplo.com">
 
-                    <p class="error-message text-red-700"></p>
-                </div>
-                <div class="mb-6 relative">
-                    <label for="senha" class="block text-gray-700 text-sm font-semibold mb-2">Senha</label>
+                <p class="error-message text-red-700"></p>
+            </div>
+            <div class="mb-6 relative">
+                <label for="senha" class="block text-gray-700 text-sm font-semibold mb-2">Senha</label>
 
-                    <div class="relative rounded-md shadow-sm">
-                        <input type="password" id="senha" name="senha"
-                            class="senha py-2 px-3 block w-full border rounded w-full py-2 px-3" placeholder="********">
-                        <div class="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer">
-                            <img class="hide-password" src="../image/icons8-hide-password-30.png" alt="hide-password">
-                            <img class="show-password hidden" src="../image/icons8-hide-password-30.png"
-                                alt="show-password">
-                        </div>
-                        <!-- <p class=" fixed error-message text-red-700"></p> -->
-                        <p class="erro-login text-red-700"></p>
+                <div class="relative rounded-md shadow-sm">
+                    <input type="password" id="senha" name="senha" class="input w-full bg-white" placeholder="********">
+                    <div class="absolute inset-y-0 right-0 pr-3 flex items-center cursor-pointer">
+                        <img class="hide-password" src="../image/icons8-hide-password-30.png" alt="hide-password">
+                        <img class="show-password hidden" src="../image/icons8-hide-password-30.png" alt="show-password">
                     </div>
-
+                    <p class="erro-login text-red-700"></p>
                 </div>
-                <div class="text-center">
-                    <button id="button-submit" type="submit"
-                        class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full py-2">Entrar</button>
-                </div>
-                <div class="text-center mt-4">
-                    <a href="<?= 'api_google.php' ?>"
-                        class="btn bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded w-full py-2 flex items-center justify-center">
-                        <img src="../image/google-icon-2048x2048-czn3g8x8.png" alt="Google Logo" class="w-6 h-6 mr-2">
-                        Entrar com Google
-                    </a>
-                </div>
-
-            </form>
-            <div class="container mx-auto px-2 mt-2">
-                <a class="hover:underline text-blue-500" href="recuperar_senha.php">Esqueceu a senha?</a>
             </div>
-            <div class="mt-4">
-                <p class="text-gray-600 px-2 ">Novo por aqui? <a href="cadastro.php"
-                        class="hover:underline text-blue-500">Crie uma conta agora</a></p>
+            <div class="text-center">
+                <button id="button-submit" type="submit" class="btn bg-black hover:bg-white text-white hover:text-black font-bold py-1 px-4 border-b-4 border-black  rounded w-full py-1">Entrar</button>
             </div>
+            <div class="text-center mt-4">
+                <a id="button-google" href="<?= 'api_google.php' ?>" class="btn bg-black hover:bg-white text-white hover:text-black font-bold py-1 px-4 border-b-4 border-black  rounded w-full py-1 flex items-center justify-center">
+                    <img src="../image/google-icon-2048x2048-czn3g8x8.png" alt="Google Logo" class="w-6 h-6 mr-2">
+                    Entrar com Google
+                </a>
+            </div>
+        </form>
+        <div class="container mx-auto px-2 mt-2">
+            <a class="hover:underline text-black-500" href="recuperar_senha.php">Esqueceu a senha?</a>
         </div>
+        <div class="mt-4">
+            <p class="text-gray-600 px-2 ">Novo por aqui? <a href="cadastro.php" class="hover:underline text-black-500">Crie uma conta agora</a></p>
+        </div>
+   
 
-    </body>
+</body>
 
 </html>
