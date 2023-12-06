532bff85879e7d402109e915dcabaca0059be47f
Natanssilva <natanssilva10@gmail.com>
2023-12-06 01:19:21 -0300

    front

--- src/api.php
+++ src/api.php
@@ -1,23 +1,40 @@
 <?php
 
-require '../vendor/autoload.php';
-require_once '../vendor/jcobhams/newsapi/src/NewsApi.php'; 
-require_once '../vendor/jcobhams/newsapi/src/NewsApiAuth.php';  // Adicione esta linha
+require '../vendor/autoload.php'; //é gerado automaticamente pelo Composer e contém as informações de carregamento automático das classes necessárias para o projeto. 
+
+//Essas linhas estão incluindo os arquivos NewsApi.php e NewsApiAuth.php da biblioteca jcobhams/newsapi. 
+// Esses arquivos contêm as definições de classes e funcionalidades necessárias para interagir com a API de notícias.
+require_once '../vendor/jcobhams/newsapi/src/NewsApi.php';
+require_once '../vendor/jcobhams/newsapi/src/NewsApiAuth.php';
 
 include_once '../biblioteca/funcoes.php';
 
 use jcobhams\NewsApi\NewsApi;
+use GuzzleHttp\Client;
 
 $key = 'd2fe47092e4344a88acacc97cfb3bf04';
 
-$newsapi = new NewsApi($key);
+$newsapi = new NewsApi($key); //Essa linha cria uma instância da classe NewsApi, passando a chave de API como argumento. 
+// A instância criada ($newsapi) será usada para fazer chamadas à API de notícias.
 
-# /v2/everything
-$all_articles = $newsapi->getEverything($q, $sources, $domains, $exclude_domains, $from, $to, $language, $sort_by,  $page_size, $page);
 
-# /v2/top-headlines
-$top_headlines = $newsapi->getTopHeadlines($q, $sources, $country, $category, $page_size, $page);
+$apiUrl =  "https://newsapi.org/v2/everything?q=apple&from=2023-12-05&to=2023-12-05&sortBy=popularity&apiKey=d2fe47092e4344a88acacc97cfb3bf04";
 
-# /v2/top-headlines/sources
-$sources = $newsapi->getSources($category, $language, $country);
+// Configurar o cliente Guzzle
+$client = new Client();  
 
+// Cria uma instância do cliente Guzzle. O Guzzle é uma biblioteca para fazer requisições HTTP de maneira eficiente em PHP.
+
+// Fazer a requisição HTTP
+$response = $client->get($apiUrl);
+// arequisição HTTP GET à API usando o cliente Guzzle. A resposta da API é armazenada na variável $response.
+
+// Obter o conteúdo da resposta
+$body = $response->getBody()->getContents();
+// pega o conteúdo da resposta (o corpo da resposta HTTP) como uma string. O corpo geralmente contém dados no formato JSON.
+
+// Converter o JSON para um array associativo
+$top_headlines = json_decode($body, true);
+
+// Exibindo os resultados (teste)
+// showArray($top_headlines);
--- src/home.php
+++ src/home.php
@@ -9,11 +9,17 @@
     <link rel="stylesheet" href="../dist/output.css">
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/daisyui@4.4.19/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
     <!-- <script src="https://cdn.tailwindcss.com"></script> -->
 </head>
 
 <body>
-
+    <?php include_once 'api.php' ?>
     <div class="navbar bg-base-100">
         <div class="navbar-start">
             <div class="dropdown">
@@ -57,6 +63,29 @@
             </div>
         </div>
     </div>
+
+    <div class="min-h-screen flex items-center justify-center">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+            <?php
+            $maxNewsToShow = 5; // Defina o número máximo de notícias que você deseja mostrar
+            $visibleNews = array_slice($top_headlines['articles'], 0, $maxNewsToShow);
+            ?>
+
+            <?php foreach ($visibleNews as $article) : ?>
+                <div class="max-w-sm mx-auto bg-white rounded-md overflow-hidden shadow-lg transform hover:scale-105 transition-transform">
+                    <img class="w-full h-40 object-cover" src="<?= $article['urlToImage'] ?>" alt="<?= $article['title'] ?>">
+                    <div class="p-4">
+                        <h2 class="text-xl font-bold mb-2"><?= $article['title'] ?></h2>
+                        <p class="text-gray-700 text-sm"><?= $article['description'] ?></p>
+                    </div>
+                    <div class="p-4 flex justify-end">
+                        <a href="<?= $article['url'] ?>" class="btn btn-primary text-sm" target="_blank">Read More</a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+
 
 </body>
 
